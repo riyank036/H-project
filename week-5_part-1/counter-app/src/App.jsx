0a658defc0bf818e6389630fb84d10fef4c75920
unknown <riyankpatel036@gmail.com>
2025-04-03 22:09:47 +0530

    update code with more react

--- week-5_part-1/counter-app/src/App.jsx
+++ week-5_part-1/counter-app/src/App.jsx
@@ -1,20 +1,24 @@
 //Hook
 import { useState } from "react";
 
-
 export default function App() {
 
-  // state, components 
+  // state
   const [count, setCount] = useState(0);
-
-  function onClickHandler() {
-  setCount(count + 1) 
-  }
-  
 
   return (
     <div>
-     <button onClick={onClickHandler}>Counter {count}</button>
+      <CustomButton count={count} setCount={setCount}></CustomButton>
     </div>
-  )
+  );
+}
+
+
+// components
+function CustomButton(props) {
+  function onClickHandler() {
+    props.setCount(props.count + 1);
+  }
+
+  return <button onClick={onClickHandler}>Counter {props.count}</button>;
 }
